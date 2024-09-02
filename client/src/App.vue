5b14153409adeb724494f7107cd103297ac9a1d8
Blake Lawatch <blakelawatch@gmail.com>
2024-09-02 13:09:52 -0600

    front end started just a bit

--- client/src/App.vue
+++ client/src/App.vue
@@ -18,6 +18,7 @@ import { AppState } from './AppState.js';
 
 <style lang="scss">
 @import '@/assets/scss/main.scss';
+
 :root {
   --main-height: calc(100vh - 32px - 64px);
 }
@@ -26,5 +27,25 @@ footer {
   display: grid;
   place-content: center;
   height: 32px;
+}
+
+.masters-yellow-bg {
+  background-color: #f4dc00;
+}
+
+.masters-green-bg {
+  background-color: #006445;
+}
+
+.masters-yellow {
+  color: #f4dc00;
+}
+
+.masters-green {
+  color: #006445;
+}
+
+.shadow {
+  text-shadow: 1px 1px 5px #f7f7f7;
 }
 </style>
