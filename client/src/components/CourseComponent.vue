b7592b7e986dd80ed07adf85ae85a73492b60f88
Blake Lawatch <blakelawatch@gmail.com>
2024-09-09 20:40:28 -0600

    idk

--- client/src/components/CourseComponent.vue
+++ client/src/components/CourseComponent.vue
@@ -1,19 +1,26 @@
 <template>
     <p>hello</p>
-    <img :src="course.img" alt="">
+    <!-- <img :src="course.img" alt=""> -->
+    <p>{{ course.name }} </p>
 </template>
 
 
 <script setup>
 import { computed } from 'vue';
-import { AppState } from '../AppState'
+import { AppState } from '../AppState';
 import { Course } from '../models/Course'
 
-props: {
+
+const props = {
     course: { type: Course, required: true }
+
 }
 
-const coverImg = computed(() => `url(${props.course?.coverImg})`)
+
+
+// const coverImg = computed(() => `url(${props.course?.img})`)
+
+
 </script>
 
 
