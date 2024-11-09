a2ecfaf43421ef07e468e4c162e3b5d95acb0bbf
Blake Lawatch <blakelawatch@gmail.com>
2024-11-09 10:54:39 -0700

    creating groups

--- client/src/pages/AccountPage.vue
+++ client/src/pages/AccountPage.vue
@@ -1,23 +1,27 @@
-<script setup>
-import { computed } from 'vue';
-import { AppState } from '../AppState.js';
-
-const account = computed(() => AppState.account)
-
-</script>
-
 <template>
   <div class="about text-center">
     <div v-if="account">
       <h1>Welcome {{ account.name }}</h1>
       <img class="rounded" :src="account.picture" alt="" />
       <p>{{ account.email }}</p>
+      <button @click="createGroup()">Create Group</button>
     </div>
     <div v-else>
       <h1>Loading... <i class="mdi mdi-loading mdi-spin"></i></h1>
     </div>
   </div>
 </template>
+
+<script setup>
+import { computed } from 'vue';
+import { AppState } from '../AppState.js';
+
+const account = computed(() => AppState.account)
+
+async function createGroup() {
+
+}
+</script>
 
 <style scoped lang="scss">
 img {
